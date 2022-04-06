f96aa1a36f59f4677002a3c21e60a112ffb5c25f
javiersrf <javiersrf@gmail.com>
2022-04-06 11:16:04 -0300

    Fix: -usando tabela pri para referencia de ultimo arquivo lido javiersrf

--- func/buscador_de_arquivos.py
+++ func/buscador_de_arquivos.py
@@ -1,5 +1,5 @@
 import os
-
+import mysql.connector
 class CaminhoDoArquivo:
     def __init__(self,caminho,data_criado,data_atualizado):
         self.caminho = caminho
@@ -13,30 +13,35 @@ class CaminhoDoArquivo:
 def myFunc(e):
   return e.data_atualizado
 def get_arquivo_pri():
+    mydb = mysql.connector.connect(host="localhost", user="smartfleet", password="smartkey",database="smartfleet")
+    con = mydb.cursor()
+    sql = "SELECT file_path FROM smartfleet.pri ORDER BY created_at DESC LIMIT 1;"
+    con.execute(sql)
+    ultimo_arquivo_lido = con.fetchone()
+    if ultimo_arquivo_lido!=None:
+        ultimo_arquivo_lido = ultimo_arquivo_lido[0] 
+    if ultimo_arquivo_lido !=None:
+        ultimo_arquivo_lido[0]
+    mydb.close()
     caminho = 'C:/prd/'
     resultado = []
     arquivos =os.listdir(caminho)    
     for arquivo in arquivos:
-        # print(arquivo)
         data_criacao = os.path.getctime(caminho+arquivo)
         data_atualizado = os.path.getmtime(caminho+arquivo)
         if arquivo.endswith(".pri") :  
             resultado.append(CaminhoDoArquivo(caminho=arquivo,data_criado=data_criacao,data_atualizado=data_atualizado))
     resultado.sort(key=myFunc,reverse=True,)
+    arquivo_mais_novo = resultado[0]
     if resultado:
-        ultimo_arquivo_gravado = None
-        try:
-            with open("last_file.txt","r") as f:
-                t = f.read()
-                ultimo_arquivo_gravado = t      
-        except FileNotFoundError as e:
-            pass
+        ultimo_arquivo_gravado = ultimo_arquivo_lido
         if ultimo_arquivo_gravado:
-            if ultimo_arquivo_gravado!= caminho +resultado[0].caminho:
-                return resultado[0]
+            if ultimo_arquivo_gravado!= arquivo_mais_novo.caminho:
+                print(ultimo_arquivo_gravado)
+                print(arquivo_mais_novo.caminho)
+                return arquivo_mais_novo
             else:
                 return None
-        return resultado[0]
+        return arquivo_mais_novo
     return None 
 
-# get_arquivo_pri()
--- func/pri_read.py
+++ func/pri_read.py
@@ -3,35 +3,30 @@ import mysql.connector
 
 
 def leitura_de_arquivo_pri(path):
+    name_path  =path
     path = 'C:/prd/'+path
     with open(path) as file:
         texto = file.read()
         pri_file = PriFile(texto)
     resume = pri_file.resume()
     valors = [list(x.values()) for x in resume["groupying"]]
-    # with open(f"{path[:-4]}_RESUMO_.csv",'a+') as f:
-    #     write = csv.writer(f)
-    #     write.writerow(["tree_amount","volume_total"])
-    #     write.writerow([resume["tree_amount"],resume["volume_total"]])
-    #     write.writerow(["id","Physical length","logs_amount","volume_total"])
-    #     write.writerows(valors)
     '''
     Ordenando os registro para qual foi o ultimo
     '''
     mydb = mysql.connector.connect(host="localhost", user="smartfleet", password="smartkey",database="smartfleet")
     con = mydb.cursor()
-    sql = "INSERT INTO `smartfleet`.`pri`(`tree_amount`,`volume_total`,`created_at`)VALUES(%s,%s,%s);"
-    val = (int(resume["tree_amount"]),int(resume["volume_total"]),pri_file.date_file.strftime('%Y-%m-%d %H:%M:%S'))
+    sql = "INSERT INTO smartfleet.pri(file_path,tree_amount,volume_total,created_at)VALUES(%s,%s,%s,%s);"
+    val = (name_path,int(resume["tree_amount"]),int(resume["volume_total"]),pri_file.date_file.strftime('%Y-%m-%d %H:%M:%S'))
     con.execute(sql,val)
     mydb.commit()
-    sql = "SELECT id from `smartfleet`.`pri` order by id desc limit 1;"
+    sql = "SELECT id from smartfleet.pri order by id desc limit 1;"
     con.execute(sql)
     resultado =con.fetchone()
     if resultado:
         ultimo_id = resultado[0] 
         valors = [list(x.values()) for x in resume["groupying"]]
         for x in valors:
-            sql = "INSERT INTO `smartfleet`.`pri_logs_resume`(`pri_id`,`product`,`lenght`,`logs_amount`,`volume_total`)VALUES(%s,%s,%s,%s,%s);"
+            sql = "INSERT INTO smartfleet.pri_logs_resume(pri_id,product,length ,logs_amount,volume_total)VALUES(%s,%s,%s,%s,%s);"
             val = (ultimo_id,x[0],x[1],x[2],int(x[3]))
             con.execute(sql,val)
             mydb.commit()
