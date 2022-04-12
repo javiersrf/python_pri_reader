baa3b9e3cfb69a386b77d6501acc3d71cba9c296
javiersrf <javiersrf@gmail.com>
2022-04-12 19:47:57 -0300

    Fix: virgula a mais quebrando a query javiersrf

--- func/pri_read.py
+++ func/pri_read.py
@@ -3,7 +3,6 @@ import mysql.connector
 
 
 def leitura_de_arquivo_pri(path):
-    print("foi a leitura")
     name_path  =path
     path = 'C:/prd/'+path
     with open(path) as file:
@@ -31,7 +30,7 @@ def leitura_de_arquivo_pri(path):
             ultimo_id = resultado[0] 
             valors = [list(x.values()) for x in resume["groupying"]]
             for x in valors:
-                sql = "INSERT INTO smartfleet.pri_logs_resume(pri_id,product,logs_amount,volume_total,min_length,max_length,avg_length)VALUES(%s,%s,%s,%s,%s,,%s,%s);"
+                sql = "INSERT INTO smartfleet.pri_logs_resume(pri_id,product,logs_amount,volume_total,min_length,max_length,avg_length)VALUES(%s,%s,%s,%s,%s,%s,%s);"
                 # pri_id,product,logs_amount,volume_total
                 val = (int(ultimo_id),str(x[0]),int(x[1]),int(x[2]),int(x[3]),int(x[4]),float(x[5]))
                 con.execute(sql,val)
